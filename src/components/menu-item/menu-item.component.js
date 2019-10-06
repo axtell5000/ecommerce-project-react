b4af339e8417f57e89a977d11341780b4f61700f
Stephen Axtell <axtell.stephen@gmail.com>
2019-10-06 19:40:42 +0200

    updating app with routing using a higher order function

--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({title, imageUrl, size}) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+  <div 
+    className={`${size} menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
     {/*We use this as background image of content so when we hover the image does not expand beyond content are */}
     <div
       className="background-image"
@@ -18,4 +21,5 @@ const MenuItem = ({title, imageUrl, size}) => (
   </div>
 );
 
-export default MenuItem;
+// Using withRouter allows us to avoid having to pops drill down, now men-uitem with have access to routes and history etc
+export default withRouter(MenuItem);
