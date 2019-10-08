2b175dc58273638a1e0645f816e4b116a184890a
Stephen Axtell <axtell.stephen@gmail.com>
2019-10-08 19:15:48 +0200

    implemented firebase utils, included is the ability to store users to db using a email and password system as well

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,34 @@ const config = {
   measurementId: "G-1BD34RQEN1"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
+
+  // here if user doesnt exit we are creating one, firstly taking our google credetials and adding it as a user
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName, 
+        email,
+        createdAt,
+        ...additionalData
+      })
+    } catch(error) {
+      console.log('error creating user', error.message);
+    }
+
+  }
+
+  return userRef;
+  
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
