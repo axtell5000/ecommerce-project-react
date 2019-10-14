0fbc8d86b3331e34588f08d88f1b93b2774df76d
Stephen Axtell <axtell.stephen@gmail.com>
2019-10-14 12:29:17 +0200

    Adding reselect to help performance and to use memoized selectors selectors

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,18 @@
+import { createSelector } from 'reselect';
+
+/* In Redux, whenever an action is called anywhere in the application, all mounted & connected components call their 
+mapStateToProps function. This is why we use Reselect. It will just return the memoized result if nothing has changed.
+
+In our code when we copy the state in our reducers and even though the values havent changed it is still considered new*/
+
+const selectCart = state => state.cart; // grabbing a piece of the state
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems], //can drill down using previous selectors
+  cartItems => cartItems.reduce((accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0)
+)
