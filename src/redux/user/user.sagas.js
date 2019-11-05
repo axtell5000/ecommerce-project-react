503d303cf09ca72a4dd53e3d494d9cefbf3faf73
Stephen Axtell <axtell.stephen@gmail.com>
2019-11-05 17:39:30 +0200

    Moved the sign up async code into sagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -5,12 +5,14 @@ import {
   SignInSuccess, 
   SignInFailure,
   signOutSuccess,
-  signOutFailure } from './user.actions';
+  signOutFailure,
+  signUpSuccess,
+  signUpFailure } from './user.actions';
 import {auth, googleProvider, createUserProfileDocument, getCurrentUser} from '../../firebase/firebase.utils'
 
-export function* getSnapshotFromUserAuth(userAuth) {
+export function* getSnapshotFromUserAuth(userAuth, additionalData) {
   try {   
-    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userRef = yield call(createUserProfileDocument, userAuth, additionalData);
     const userSnapshot = yield userRef.get();
     yield put(SignInSuccess({ id: userSnapshot.id, ...userSnapshot.data()}));
     console.log(userRef);
@@ -56,6 +58,19 @@ export function* signOut() {
   }
 }
 
+export function* signUp({ payload: { email, password, displayName } }) {
+  try {
+    const { user } = yield auth.createUserWithEmailAndPassword(email, password);
+    yield put(signUpSuccess({ user, additionalData: { displayName } }));
+  } catch (error) {
+    yield put(signUpFailure(error));
+  }
+}
+
+export function* signInAfterSignUp({ payload: { user, additionalData } }) {
+  yield getSnapshotFromUserAuth(user, additionalData);
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -72,10 +87,20 @@ export function* onSignoutOutStart() {
   yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut)
 }
 
+export function* onSignUpStart() {
+  yield takeLatest(UserActionTypes.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess() {
+  yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS, signInAfterSignUp);
+}
+
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart), 
     call(onEmailSignInStart), 
     call(isUserAuthenticated),
-    call(onSignoutOutStart)]);
+    call(onSignoutOutStart),
+    call(onSignUpStart),
+    call(onSignUpSuccess)]);
 }
