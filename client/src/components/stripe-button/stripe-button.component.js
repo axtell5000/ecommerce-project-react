ac1f9471532cc3a4aca562873d6b3612c5b557e2
Stephen Axtell <axtell.stephen@gmail.com>
2019-11-11 16:57:44 +0200

    restructured project to have a backend as well to handle stripe payments

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_vXsBH3Mkg64zylEBbEIs4xSY00QPvN1I1Y';
 
   const onToken = token => {
-    console.log(token);
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token: token
+      }
+    }).then(response => {
+      alert('Payment successful')
+    }).catch(error => {
+      console.log('Payment error: ', JSON.parse(error));
+      alert('THere was an issue with your payment. Please use the provided credit card');
+    })
   };
 
   return (
